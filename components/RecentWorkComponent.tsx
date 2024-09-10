f11ce1680d04ee60a31c195700462482204d1e68
paigauresh <paigauresh@gmail.com>
2024-09-10 21:12:45 +0530

    button text extrabolded

--- components/RecentWorkComponent.tsx
+++ components/RecentWorkComponent.tsx
@@ -38,7 +38,7 @@ const RecentWorkComponent = () => {
                 <div className="mt-8 text-center">
                     <Link
                         href="/Work"
-                        className="bg-green-200 text-black py-2 px-6 text-lg hover:bg-transparent hover:text-green-200 hover:border-green-200 border-2 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white shadow-md hover:shadow-lg"
+                        className="bg-green-200 font-extrabold text-black py-2 px-6 text-lg hover:bg-transparent hover:text-green-200 hover:border-green-200 border-2 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white shadow-md hover:shadow-lg"
                     >
                         View All
                     </Link>
