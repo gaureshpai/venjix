47efbc71750251dd3199384e2cdc358f067385ea
paigauresh <paigauresh@gmail.com>
2024-09-10 14:18:59 +0530

    custom tailwind classes added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      screens: {
+        '3xl': '1920px',
+        '4xl': '2560px',
+        '5xl': '3200px',
+        '6xl': '3840px',
+        '7xl': '4480px',
+        '8xl': '5120px',
+        '9xl': '5760px',
+        '10xl': '6400px',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        Lora: ['Lora', 'serif'],
+        Mont: ['Montserrat', 'sans - serif'],
+      }
+    }
   },
   plugins: [],
 };
+
 export default config;
