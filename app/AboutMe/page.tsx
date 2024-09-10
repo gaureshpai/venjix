e35ddc981878873128bb38d9f4955e726a9292f5
paigauresh <paigauresh@gmail.com>
2024-09-10 21:11:58 +0530

    AboutMe page done

--- app/AboutMe/page.tsx
+++ app/AboutMe/page.tsx
@@ -1,12 +1,19 @@
+import AboutMeVid from '@/components/AboutMeVid'
+import BusinessInNumbers from '@/components/BusinessInNumbers'
+import Experience from '@/components/Experience'
+import Intro from '@/components/Intro'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='min-h-[80vh]'>
       <div className='min-h-[10vh]'></div>
-      page
+      <Intro/>
+      <BusinessInNumbers/>
+      <Experience/>
+      <AboutMeVid/>
     </div>
   )
 }
 
-export default page
+export default page;
