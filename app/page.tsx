c8ca01c2bbbc4971a0db4fe52bcb7988e99ee9ff
paigauresh <paigauresh@gmail.com>
2024-09-10 20:35:16 +0530

    new components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import VideoContainer from "@/components/VideoContainer";
 import IntroductionComponent from "@/components/IntroductionComponent";
+import EditorProfileComponent from "@/components/EditorProfileComponent";
+import ServicesComponent from "@/components/ServicesComponent";
+import RecentWorkComponent from "@/components/RecentWorkComponent";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
       <VideoContainer />
       <IntroductionComponent/>
+      <EditorProfileComponent/>
+      <ServicesComponent/>
+      <RecentWorkComponent/>
     </main>
   );
 }
