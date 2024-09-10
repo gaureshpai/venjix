c701357b19117654d9759b11234b4662553a36eb
paigauresh <paigauresh@gmail.com>
2024-09-11 00:03:11 +0530

    new service page component rendering

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -1,10 +1,17 @@
+import ServiceContact from '@/components/ServiceContact'
+import ServiceInfo from '@/components/ServiceInfo'
+import ServiceVideo from '@/components/ServiceVideo'
+import WhyBookService from '@/components/WhyBookService'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='min-h-[80vh]'>
       <div className='min-h-[10vh]'></div>
-      page
+      <ServiceInfo/>
+      <ServiceContact/>
+      <ServiceVideo/>
+      <WhyBookService/>
     </div>
   )
 }
