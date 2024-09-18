f98d1d0e794dabb3ba4020789ab82c7211a4cbf8
paigauresh <paigauresh@gmail.com>
2024-09-19 00:02:07 +0530

    Video replaced to client response vid

--- components/AboutMeVid.tsx
+++ components/AboutMeVid.tsx
@@ -1,17 +1,22 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AboutMeVid = () => {
     return (
-        <div className="flex justify-center items-center bg-black md:py-16 md:px-36 lg:px-40">
+        <div className="relative flex justify-center items-center bg-black py-12 px-4 md:py-16 md:px-8 lg:px-12">
+            <p className="absolute text-2xl top-4 left-50 text-white bg-opacity-75 px-4 py-2 rounded-lg shadow-lg">
+                Client's Happy Reaction on my work
+            </p>
             <video
-                src="images/bg.mp4"
-                autoPlay
-                loop
-                muted
-                className="w-full shadow-2xl"
+                src="images/ClientResponse.mp4"
+                controls
+                className="w-full max-w-4xl max-h-[70vh] shadow-lg rounded-lg border border-gray-800"
             >
                 Your browser does not support the video tag.
             </video>
+            <Link href={'https://youtu.be/ShRte3EcSuA?si=gqfLpKX-LOkc9J2o'} className=" absolute bottom-1 bg-[#97C584] text-black py-2 font-bold px-4 md:py-2 md:px-6 text-base md:text-lg hover:bg-transparent hover:text-[#97C584] border-[#97C584] border-2 transition duration-300 ease-in-out shadow-md hover:shadow-lg">
+                View on youtube
+            </Link>
         </div>
     );
 };
