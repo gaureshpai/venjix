0802325aa095f502c6cd146ab2e26c4e7e9b785f
paigauresh <paigauresh@gmail.com>
2024-09-23 01:20:55 +0530

    json external src fetch implemented with few styling chnages and data with src

--- components/ServiceInfo.tsx
+++ components/ServiceInfo.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
+import serv from '@/JSON/services.json'
 
 const ServiceInfo = () => {
-    const services = [
-        {
-            title: 'Editing',
-            description: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.',
-            image: '/images/s1.jpg'
-        },
-        {
-            title: 'Color Correction',
-            description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur.',
-            image: '/images/s2.jpg'
-        },
-        {
-            title: 'Sound Design',
-            description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.',
-            image: '/images/s3.jpg'
-        }
-    ];
+    const services = serv.services;
 
     return (
         <div className="bg-black text-white py-16 px-8 md:px-16 lg:px-24">
